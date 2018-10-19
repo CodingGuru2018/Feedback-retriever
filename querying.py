81fe5d640bebbc0243c51bf722308bf947f8d721
Pratham1812 <32198580+Pratham1812@users.noreply.github.com>
2018-10-18 21:47:14 -0700

    Update querying.py

--- querying.py
+++ querying.py
@@ -1,10 +1,18 @@
-import time
-from pandas import DataFrame
-import sqlite3
+#to query data from the database created in main.py
+
+try:
+  import time
+  from pandas import DataFrame
+  import sqlite3
+except ImportError:
+  print("All modules not found")
+#connecting to database created using main.py
 db = sqlite3.connect("review.db")
 
 cursor = db.cursor()
 cursor.execute('SELECT * FROM review ')
+
+#arranging the data in a table using DataFrame module inside Pandas
 print(DataFrame(cursor.fetchall(), columns=['Name','Class','email-id','Review','Suggestions']))
 
 cursor.close()
