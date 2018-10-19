b63053a0f0896a1f9dea8b31a2ede98ecb6e0920
Pratham1812 <32198580+Pratham1812@users.noreply.github.com>
2018-10-18 21:42:23 -0700

    Update and rename test.py to graph.py

--- graph.py
+++ graph.py
@@ -1,7 +1,13 @@
-import sqlite3
-import numpy as np
-from matplotlib import pyplot as plt
 
+
+try:
+  import sqlite3
+  import numpy as np
+  from matplotlib import pyplot as plt
+except ImportError:
+  print("Install all the modules first ie numpy,matplotlib ")
+  
+#connecting to the database created using main.py  
 db = sqlite3.connect('review.db')
 
 cursor = db.cursor()
@@ -11,14 +17,14 @@ t = cursor.execute('SELECT grade FROM review ORDER BY grade')
 x = np.sort(np.array((cursor.fetchall())))
 
 
-print(x)
 
 t2 = cursor.execute('SELECT review FROM review ORDER BY grade')
 
+
 y = np.sort(np.array(cursor.fetchall()))
 np.sort(y)
-print(y)
 
+#PLOTTING THE GRAPH USING MATPLOTLIB
 plt.title("REVIEWS OF STUDENT V/S GRADE GRAPH")
 plt.xlabel('GRADE')
 plt.ylabel('REVIEW')
